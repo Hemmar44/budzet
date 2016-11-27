42dbd1da3a623540f7ab65114cc5948a3e946640
Hemmar44 <hedrzak.marcin@gmail.com>
2016-11-27 09:09:44 +0100

    Przygototowanie pliku do budowy wymaganej bazy danych

---
+++ createdatabase.php
@@ -0,0 +1,36 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+	<meta charset="utf-8" />
+
+	
+	<title>Stwórz tabele</title>
+	
+</head>
+<body>
+<div><span>Stwórz bazę danych potzebną do prawidłowego działania aplikacji</span></div>
+<form method="POST">
+	<table>
+		<thead>
+			<tr>
+				<th>Pozycja</th><th>Dane</th>
+			</tr>
+			<tr>
+				<td>Server name</td><td><input type="text"/></td>
+			</tr>
+			<tr>
+				<td>Username</td><td><input type="text"/></td>
+			</tr>
+			<tr>
+				<td>Password</td><td><input type="password"/></td>
+			</tr>
+			<tr>
+				<td>Database</td><td><input type="text" value="budget"></td>
+			</tr>
+		</thead>
+	</table>
+	<input type="submit" value="Utwórz Bazę"/>
+
+</form>
+</body>
+</html>
